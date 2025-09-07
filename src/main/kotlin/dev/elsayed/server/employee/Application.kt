9928848b13e4cbb3b58f91cf6259217d99ad4f02
Elsayed Magdy <saikoo1239@gmail.com>
2025-09-08 00:00:37 +0300

    first commit

---
+++ src/main/kotlin/dev/elsayed/server/employee/Application.kt
@@ -0,0 +1,11 @@
+package dev.elsayed.server.employee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
