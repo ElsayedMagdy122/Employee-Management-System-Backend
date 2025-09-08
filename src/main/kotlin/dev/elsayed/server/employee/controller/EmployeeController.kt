75bf850bc925d38f404407c0c164c04ef79e7d88
Elsayed Magdy <saikoo1239@gmail.com>
2025-09-08 08:36:18 +0300

    feat : add all crud operations to EmployeeController

--- src/main/kotlin/dev/elsayed/server/employee/controller/EmployeeController.kt
+++ src/main/kotlin/dev/elsayed/server/employee/controller/EmployeeController.kt
@@ -2,38 +2,48 @@ package dev.elsayed.server.employee.controller
 
 import dev.elsayed.server.employee.entity.Employee
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/employees")
 class EmployeeController {
-    val employees = listOf(
-        Employee(
-            firstName = "Elsayed",
-            lastName = "Magdy",
-            email = "email1@test.com",
-            phoneNumber = "123",
-            position = "Dev"
-        ),
-        Employee(
-            firstName = "Ahmed",
-            lastName = "Ali",
-            email = "email2@test.com",
-            phoneNumber = "456",
-            position = "Tester"
-        ),
-    )
+    val employees = mutableListOf<Employee>()
 
     @GetMapping
     fun findAll(): ResponseEntity<List<Employee>> {
         return ResponseEntity.ok(employees)
     }
 
-    @GetMapping("/{name}")
-    fun findOne(@PathVariable name: String): ResponseEntity<Employee> {
-        return ResponseEntity.ok(employees.find { it.firstName.equals(name, ignoreCase = true) })
+    @GetMapping("/{id}")
+    fun findOne(@PathVariable id: UUID): ResponseEntity<Employee> {
+        return ResponseEntity.ok(employees.find { it.id == id })
+    }
+
+    @PostMapping
+    fun create(@RequestBody employee: Employee): ResponseEntity<Employee> {
+        employees.add(employee)
+        return ResponseEntity.ok(employee)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteOne(@PathVariable id: UUID): ResponseEntity<Employee> {
+        employees.removeIf { it.id == id }
+        return ResponseEntity.noContent().build()
+    }
+
+    @PutMapping("/{id}")
+    fun update(
+        @PathVariable id: UUID,
+        @RequestBody employee: Employee
+    ): ResponseEntity<Employee> {
+        val index = employees.indexOfFirst { it.id == id }
+        return if (index != -1) {
+            val updatedEmployee = employee.copy(id = id)
+            employees[index] = updatedEmployee
+            ResponseEntity.ok(updatedEmployee)
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 }
--- src/main/kotlin/dev/elsayed/server/employee/entity/Employee.kt
+++ src/main/kotlin/dev/elsayed/server/employee/entity/Employee.kt
@@ -1,15 +1,15 @@
 package dev.elsayed.server.employee.entity
 
 import java.time.LocalDate
-import java.util.UUID
+import java.util.*
 
 data class Employee(
-    val id : UUID = UUID.randomUUID(),
+    val id: UUID = UUID.randomUUID(),
+    val departmentId: UUID = UUID.randomUUID(),
     val firstName: String,
     val lastName: String,
     val email: String,
     val phoneNumber: String,
-    val hireDate: LocalDate = LocalDate.now(),
     val position: String,
-    val departmentId: UUID = UUID.randomUUID(),
+    val hireDate: LocalDate = LocalDate.now()
 )
